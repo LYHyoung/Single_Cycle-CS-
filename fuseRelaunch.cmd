49a32fb6b74994d55939c58cc9ca92834cd41642
LYHyoung <lyh4186@naver.com>
2021-04-30 07:48:50 -0700

    Add files via upload

---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/Xilinx/workspace/SingleCycle/tb_SingleCycle_isim_beh.exe" -prj "D:/Xilinx/workspace/SingleCycle/tb_SingleCycle_beh.prj" "work.tb_SingleCycle" "work.glbl" 
